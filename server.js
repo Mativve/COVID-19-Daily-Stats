ca80afc4d3869cc831375dcbc2abdae31c921f58
Mateusz Świder <mativve@gmail.com>
2021-10-26 18:17:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+const path = require('path');
+const serveStatic = require('serve-static');
 const enforce = require('express-sslify');
 
 const app = express();
-
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+const port = process.env.PORT || 5000;
 
 app.use(history({
-    disableDotRule: true,
     verbose: true
 }));
 
-app.use(serveStatic(__dirname + '/dist'));
+app.use(serveStatic(path.join(__dirname, '/dist');
 
-app.listen(process.env.PORT || 5000);
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+
+app.listen(port, () => {
+    console.log('Server started at http://localhost:'+port);
+});
